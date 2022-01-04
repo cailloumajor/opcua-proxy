841f6d6ca1b87aa0d0e1eaf5839672ba3d026a76
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-01-04 14:30:10 +0000

    chore: create opcua interfaces

--- internal/opcua/opcua.go
+++ internal/opcua/opcua.go
@@ -1,5 +1,45 @@
 package opcua
 
+import (
+	"context"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/monitor"
+	"github.com/gopcua/opcua/ua"
+)
+
+//go:generate moq -out mocks_test.go -pkg opcua_test . EndpointsGetter EndpointSelector Client ClientCreator NodeMonitor NodeMonitorCreator
+
+// EndpointsGetter models an OPC-UA server endpoints getter
+type EndpointsGetter interface {
+	GetEndpoints(ctx context.Context, endpoint string, opts ...opcua.Option) ([]*ua.EndpointDescription, error)
+}
+
+// EndpointSelector models an OPC-UA server endpoint selector
+type EndpointSelector interface {
+	SelectEndpoint(endpoints []*ua.EndpointDescription, policy string, mode ua.MessageSecurityMode) *ua.EndpointDescription
+}
+
+// Client models an OPC-UA client
+type Client interface {
+	Connect(context.Context) (err error)
+}
+
+// ClientCreator models an OPC-UA client creator
+type ClientCreator interface {
+	NewClient(endpoint string, opts ...opcua.Option) Client
+}
+
+// NodeMonitor models an OPC-UA node monitor
+type NodeMonitor interface {
+	ChanSubscribe(context.Context, *opcua.SubscriptionParameters, chan<- *monitor.DataChangeMessage, ...string) (*monitor.Subscription, error)
+}
+
+// NodeMonitorCreator models an OPC-UA node monitor creator
+type NodeMonitorCreator interface {
+	NewNodeMonitor(client Client) (NodeMonitor, error)
+}
+
 // Config holds the OPC-UA part of the configuration
 type Config struct {
 	ServerURL string
@@ -7,4 +47,15 @@ type Config struct {
 	Password  string `envconfig:"optional"`
 	CertFile  string `envconfig:"optional"`
 	KeyFile   string `envconfig:"optional"`
+}
+
+// NewMonitor creates an OPC-UA node monitor
+func NewMonitor(ctx context.Context, config *Config, eg EndpointsGetter, es EndpointSelector, cc ClientCreator, nmc NodeMonitorCreator) (NodeMonitor, error) {
+	ep, _ := eg.GetEndpoints(ctx, config.ServerURL)
+
+	es.SelectEndpoint(ep, "None", ua.MessageSecurityModeNone)
+
+	cc.NewClient("blabla")
+
+	return nil, nil
 }
