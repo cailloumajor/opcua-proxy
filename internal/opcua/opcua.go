5cef762e0907609a08d36e1e2d07c3f5d928d14a
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-01-05 14:32:52 +0000

    chore: implement opcua.NewMonitor

--- internal/opcua/opcua.go
+++ internal/opcua/opcua.go
@@ -2,13 +2,23 @@ package opcua
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gopcua/opcua"
 	"github.com/gopcua/opcua/monitor"
 	"github.com/gopcua/opcua/ua"
 )
 
-//go:generate moq -out mocks_test.go -pkg opcua_test . Client NodeMonitor Dependencies
+//go:generate moq -out opcua_mocks_test.go . Client NodeMonitor NewMonitorDeps
+
+// Config holds the OPC-UA part of the configuration
+type Config struct {
+	ServerURL string
+	User      string `envconfig:"optional"`
+	Password  string `envconfig:"optional"`
+	CertFile  string `envconfig:"optional"`
+	KeyFile   string `envconfig:"optional"`
+}
 
 // Client models an OPC-UA client
 type Client interface {
@@ -20,30 +30,62 @@ type NodeMonitor interface {
 	ChanSubscribe(context.Context, *opcua.SubscriptionParameters, chan<- *monitor.DataChangeMessage, ...string) (*monitor.Subscription, error)
 }
 
-// Dependencies models the dependencies of NewMonitor
-type Dependencies interface {
+// NewMonitorDeps models the dependencies of NewMonitor
+type NewMonitorDeps interface {
 	GetEndpoints(ctx context.Context, endpoint string, opts ...opcua.Option) ([]*ua.EndpointDescription, error)
 	SelectEndpoint(endpoints []*ua.EndpointDescription, policy string, mode ua.MessageSecurityMode) *ua.EndpointDescription
+	AuthUsername(user, pass string) opcua.Option
+	CertificateFile(filename string) opcua.Option
+	PrivateKeyFile(filename string) opcua.Option
+	SecurityFromEndpoint(ep *ua.EndpointDescription, authType ua.UserTokenType) opcua.Option
 	NewClient(endpoint string, opts ...opcua.Option) Client
 	NewNodeMonitor(client Client) (NodeMonitor, error)
 }
 
-// Config holds the OPC-UA part of the configuration
-type Config struct {
-	ServerURL string
-	User      string `envconfig:"optional"`
-	Password  string `envconfig:"optional"`
-	CertFile  string `envconfig:"optional"`
-	KeyFile   string `envconfig:"optional"`
-}
-
 // NewMonitor creates an OPC-UA node monitor
-func NewMonitor(ctx context.Context, config *Config, eg EndpointsGetter, es EndpointSelector, cc ClientCreator, nmc NodeMonitorCreator) (NodeMonitor, error) {
-	ep, _ := eg.GetEndpoints(ctx, config.ServerURL)
+func NewMonitor(ctx context.Context, cfg *Config, deps NewMonitorDeps) (NodeMonitor, error) {
+	eps, err := deps.GetEndpoints(ctx, cfg.ServerURL)
+	if err != nil {
+		return nil, fmt.Errorf("NewMonitor: error getting endpoints: %w", err)
+	}
 
-	es.SelectEndpoint(ep, "None", ua.MessageSecurityModeNone)
+	var opts []opcua.Option
 
-	cc.NewClient("blabla")
+	var p string
+	var msm ua.MessageSecurityMode
+	if cfg.CertFile != "" || cfg.KeyFile != "" {
+		p = "Basic256Sha256"
+		msm = ua.MessageSecurityModeSignAndEncrypt
+		opts = append(opts, deps.CertificateFile(cfg.CertFile), deps.PrivateKeyFile(cfg.KeyFile))
+	} else {
+		p = "None"
+		msm = ua.MessageSecurityModeNone
+	}
 
-	return nil, nil
+	ep := deps.SelectEndpoint(eps, p, msm)
+	if ep == nil {
+		return nil, fmt.Errorf("NewMonitor: failed to select an endpoint")
+	}
+
+	var utt ua.UserTokenType
+	if cfg.User == "" {
+		utt = ua.UserTokenTypeAnonymous
+	} else {
+		utt = ua.UserTokenTypeUserName
+		opts = append(opts, deps.AuthUsername(cfg.User, cfg.Password))
+	}
+	opts = append(opts, deps.SecurityFromEndpoint(ep, utt))
+
+	c := deps.NewClient(ep.EndpointURL, opts...)
+
+	if err := c.Connect(ctx); err != nil {
+		return nil, fmt.Errorf("NewMonitor: failed to connect: %w", err)
+	}
+
+	m, err := deps.NewNodeMonitor(c)
+	if err != nil {
+		return nil, fmt.Errorf("NewMonitor: failed to create a node monitor: %w", err)
+	}
+
+	return m, nil
 }
---
+++ internal/opcua/opcua_test.go
@@ -0,0 +1,305 @@
+package opcua_test
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+
+	. "github.com/cailloumajor/opcua-centrifugo/internal/opcua"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/ua"
+)
+
+var errTesting = errors.New("general error for testing")
+
+type authUserNameCall struct {
+	calls uint
+	user  string
+	pass  string
+}
+
+type certificateFileCall struct {
+	calls    uint
+	filename string
+}
+
+type privateKeyFileCall struct {
+	calls    uint
+	filename string
+}
+
+func TestNewMonitorSuccess(t *testing.T) {
+	cases := []struct {
+		name                       string
+		config                     Config
+		expectSelectEndpointPolicy string
+		expectSelectEndpointMode   ua.MessageSecurityMode
+		expectAuthUserNameCalls    authUserNameCall
+		expectCertificateFileCalls certificateFileCall
+		expectPrivateKeyFileCalls  privateKeyFileCall
+		expectUserTokenType        ua.UserTokenType
+		expectNewClientOptsCount   int
+	}{
+		{
+			name:                       "SuccessWithoutAuthWithoutEncryption",
+			config:                     Config{},
+			expectSelectEndpointPolicy: "None",
+			expectSelectEndpointMode:   ua.MessageSecurityModeNone,
+			expectAuthUserNameCalls:    authUserNameCall{},
+			expectCertificateFileCalls: certificateFileCall{},
+			expectPrivateKeyFileCalls:  privateKeyFileCall{},
+			expectUserTokenType:        ua.UserTokenTypeAnonymous,
+			expectNewClientOptsCount:   1,
+		},
+		{
+			name:                       "SuccessWithAuthWithoutEncryption",
+			config:                     Config{User: "user1", Password: "pass1"},
+			expectSelectEndpointPolicy: "None",
+			expectSelectEndpointMode:   ua.MessageSecurityModeNone,
+			expectAuthUserNameCalls:    authUserNameCall{calls: 1, user: "user1", pass: "pass1"},
+			expectCertificateFileCalls: certificateFileCall{},
+			expectPrivateKeyFileCalls:  privateKeyFileCall{},
+			expectUserTokenType:        ua.UserTokenTypeUserName,
+			expectNewClientOptsCount:   2,
+		},
+		{
+			name:                       "SuccessWithoutAuthWithEncryption",
+			config:                     Config{CertFile: "cert1", KeyFile: "key1"},
+			expectSelectEndpointPolicy: "Basic256Sha256",
+			expectSelectEndpointMode:   ua.MessageSecurityModeSignAndEncrypt,
+			expectAuthUserNameCalls:    authUserNameCall{},
+			expectCertificateFileCalls: certificateFileCall{calls: 1, filename: "cert1"},
+			expectPrivateKeyFileCalls:  privateKeyFileCall{calls: 1, filename: "key1"},
+			expectUserTokenType:        ua.UserTokenTypeAnonymous,
+			expectNewClientOptsCount:   3,
+		},
+		{
+			name:                       "SuccessWithAuthWithEncryption",
+			config:                     Config{User: "user2", Password: "pass2", CertFile: "cert2", KeyFile: "key2"},
+			expectSelectEndpointPolicy: "Basic256Sha256",
+			expectSelectEndpointMode:   ua.MessageSecurityModeSignAndEncrypt,
+			expectAuthUserNameCalls:    authUserNameCall{calls: 1, user: "user2", pass: "pass2"},
+			expectCertificateFileCalls: certificateFileCall{calls: 1, filename: "cert2"},
+			expectPrivateKeyFileCalls:  privateKeyFileCall{calls: 1, filename: "key2"},
+			expectUserTokenType:        ua.UserTokenTypeUserName,
+			expectNewClientOptsCount:   4,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				authUserNameCalls    authUserNameCall
+				certificateFileCalls certificateFileCall
+				privateKeyFileCalls  privateKeyFileCall
+			)
+			mockedEndpointDescription := &ua.EndpointDescription{EndpointURL: "selectedEndpointURL"}
+			mockedClient := &ClientMock{
+				ConnectFunc: func(contextMoqParam context.Context) error {
+					return nil
+				},
+			}
+			mockedNodeMonitor := &NodeMonitorMock{}
+			mockedNewMonitorDeps := &NewMonitorDepsMock{
+				GetEndpointsFunc: func(ctx context.Context, endpoint string, opts ...opcua.Option) ([]*ua.EndpointDescription, error) {
+					return []*ua.EndpointDescription{}, nil
+				},
+				SelectEndpointFunc: func(endpoints []*ua.EndpointDescription, policy string, mode ua.MessageSecurityMode) *ua.EndpointDescription {
+					return mockedEndpointDescription
+				},
+				AuthUsernameFunc: func(user, pass string) opcua.Option {
+					authUserNameCalls.calls++
+					authUserNameCalls.user = user
+					authUserNameCalls.pass = pass
+					return func(c *opcua.Config) {}
+				},
+				CertificateFileFunc: func(filename string) opcua.Option {
+					certificateFileCalls.calls++
+					certificateFileCalls.filename = filename
+					return func(c *opcua.Config) {}
+				},
+				PrivateKeyFileFunc: func(filename string) opcua.Option {
+					privateKeyFileCalls.calls++
+					privateKeyFileCalls.filename = filename
+					return func(c *opcua.Config) {}
+				},
+				SecurityFromEndpointFunc: func(ep *ua.EndpointDescription, authType ua.UserTokenType) opcua.Option {
+					return func(c *opcua.Config) {}
+				},
+				NewClientFunc: func(endpoint string, opts ...opcua.Option) Client {
+					return mockedClient
+				},
+				NewNodeMonitorFunc: func(client Client) (NodeMonitor, error) {
+					return mockedNodeMonitor, nil
+				},
+			}
+
+			tc.config.ServerURL = "serverURL"
+
+			// Target of the test
+			m, err := NewMonitor(context.Background(), &tc.config, mockedNewMonitorDeps)
+
+			// Assertions about GetEndpoints
+			if got, want := len(mockedNewMonitorDeps.GetEndpointsCalls()), 1; got != want {
+				t.Errorf("GetEndpoints call count: want %d, got %d", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.GetEndpointsCalls()[0].Endpoint, "serverURL"; got != want {
+				t.Errorf("GetEndpoints endpoint argument: want %q, got %q", want, got)
+			}
+			if got, want := len(mockedNewMonitorDeps.GetEndpointsCalls()[0].Opts), 0; got != want {
+				t.Errorf("GetEndpoints opts argument length: want %d, got %d", want, got)
+			}
+			// Assertions about SelectEndpoint
+			if got, want := len(mockedNewMonitorDeps.SelectEndpointCalls()), 1; got != want {
+				t.Errorf("SelectEndpoint call count: want %d, got %d", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.SelectEndpointCalls()[0].Policy, tc.expectSelectEndpointPolicy; got != want {
+				t.Errorf("SelectEndpoint policy argument: want %q, got %q", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.SelectEndpointCalls()[0].Mode, tc.expectSelectEndpointMode; got != want {
+				t.Errorf("SelectEndpoint mode argument: want %q, got %q", want, got)
+			}
+			// Assertions about AuthUserName
+			if got, want := authUserNameCalls, tc.expectAuthUserNameCalls; !reflect.DeepEqual(got, want) {
+				t.Errorf("AuthUserName calls: want %#v, got %#v", want, got)
+			}
+			// Assertions about CertificateFile
+			if got, want := certificateFileCalls, tc.expectCertificateFileCalls; !reflect.DeepEqual(got, want) {
+				t.Errorf("CertificateFile calls: want %#v, got %#v", want, got)
+			}
+			// Assertions about PrivateKeyFile
+			if got, want := privateKeyFileCalls, tc.expectPrivateKeyFileCalls; !reflect.DeepEqual(got, want) {
+				t.Errorf("PrivateKeyFile calls: want %#v, got %#v", want, got)
+			}
+			// Assertions about SecurityFromEndpoint
+			if got, want := len(mockedNewMonitorDeps.SecurityFromEndpointCalls()), 1; !reflect.DeepEqual(got, want) {
+				t.Errorf("SecurityFromEndpoint call count: want %d, got %d", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.SecurityFromEndpointCalls()[0].Ep, mockedEndpointDescription; got != want {
+				t.Errorf("SecurityFromEndpoint ep argument: want %#v, got %#v", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.SecurityFromEndpointCalls()[0].AuthType, tc.expectUserTokenType; got != want {
+				t.Errorf("SecurityFromEndpoint authType argument: want %q, got %q", want, got)
+			}
+			// Assertions about NewClient
+			if got, want := len(mockedNewMonitorDeps.NewClientCalls()), 1; got != want {
+				t.Errorf("NewClient call count: want %d, got %d", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.NewClientCalls()[0].Endpoint, "selectedEndpointURL"; got != want {
+				t.Errorf("NewClient endpoint argument: want %q, got %q", want, got)
+			}
+			if got, want := len(mockedNewMonitorDeps.NewClientCalls()[0].Opts), tc.expectNewClientOptsCount; got != want {
+				t.Errorf("NewClient opts arguments count: want %+v, got %+v", want, got)
+			}
+			// Assertions about Client.Connect
+			if got, want := len(mockedClient.ConnectCalls()), 1; got != want {
+				t.Errorf("Client.Connect call count: want %d, got %d", want, got)
+			}
+			// Assertions about NewNodeMonitor
+			if got, want := len(mockedNewMonitorDeps.NewNodeMonitorCalls()), 1; got != want {
+				t.Errorf("NewNodeMonitor call count: want %d, got %d", want, got)
+			}
+			if got, want := mockedNewMonitorDeps.NewNodeMonitorCalls()[0].Client, mockedClient; got != want {
+				t.Errorf("NewNodeMonitor client argument: want %+v, got %+v", want, got)
+			}
+			// Assertions about NewMonitor
+			if got, want := m, mockedNodeMonitor; got != want {
+				t.Errorf("NewMonitor return: want %+v, got %+v", want, got)
+			}
+			if got := err; got != nil {
+				t.Errorf("NewMonitor error return: want nil, got %v", got)
+			}
+		})
+	}
+}
+
+func TestNewMonitorError(t *testing.T) {
+	cases := []struct {
+		name                string
+		getEndpointsError   bool
+		selectEndpointNil   bool
+		clientConnectError  bool
+		newNodeMonitorError bool
+	}{
+		{
+			name:                "GetEndpointsError",
+			getEndpointsError:   true,
+			selectEndpointNil:   false,
+			clientConnectError:  false,
+			newNodeMonitorError: false,
+		},
+		{
+			name:                "SelectEndpointNil",
+			getEndpointsError:   false,
+			selectEndpointNil:   true,
+			clientConnectError:  false,
+			newNodeMonitorError: false,
+		},
+		{
+			name:                "ClientConnectError",
+			getEndpointsError:   false,
+			selectEndpointNil:   false,
+			clientConnectError:  true,
+			newNodeMonitorError: false,
+		},
+		{
+			name:                "NewNodeMonitorError",
+			getEndpointsError:   false,
+			selectEndpointNil:   false,
+			clientConnectError:  false,
+			newNodeMonitorError: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockedClient := &ClientMock{
+				ConnectFunc: func(contextMoqParam context.Context) error {
+					if tc.clientConnectError {
+						return errTesting
+					}
+					return nil
+				},
+			}
+			mockedNewMonitorDeps := &NewMonitorDepsMock{
+				GetEndpointsFunc: func(ctx context.Context, endpoint string, opts ...opcua.Option) ([]*ua.EndpointDescription, error) {
+					if tc.getEndpointsError {
+						return nil, errTesting
+					}
+					return []*ua.EndpointDescription{}, nil
+				},
+				SelectEndpointFunc: func(endpoints []*ua.EndpointDescription, policy string, mode ua.MessageSecurityMode) *ua.EndpointDescription {
+					if tc.selectEndpointNil {
+						return nil
+					}
+					return &ua.EndpointDescription{}
+				},
+				SecurityFromEndpointFunc: func(ep *ua.EndpointDescription, authType ua.UserTokenType) opcua.Option {
+					return func(c *opcua.Config) {}
+				},
+				NewClientFunc: func(endpoint string, opts ...opcua.Option) Client {
+					return mockedClient
+				},
+				NewNodeMonitorFunc: func(client Client) (NodeMonitor, error) {
+					if tc.newNodeMonitorError {
+						return nil, errTesting
+					}
+					return &NodeMonitorMock{}, nil
+				},
+			}
+
+			// Target of the test
+			m, err := NewMonitor(context.Background(), &Config{}, mockedNewMonitorDeps)
+
+			// Assertions about NewMonitor
+			if got := m; got != nil {
+				t.Errorf("NewMonitor return: want nil, got %+v", got)
+			}
+			if err == nil {
+				t.Error("NewMonitor error return: want an error, got nil")
+			}
+		})
+	}
+}
