182e7433103e5a9847addfab56f37c7024e51fe3
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-04-27 13:46:43 +0200

    feat: add heartbeat publication

--- integration_tests/cypress/integration/spec.js
+++ integration_tests/cypress/integration/spec.js
@@ -1,4 +1,5 @@
 const subscriptionInterval = 500
+const ns = Cypress.env("CENTRIFUGO_NAMESPACE")
 
 before(() => {
   cy.request({
@@ -10,6 +11,7 @@ before(() => {
 describe("Integration tests page", () => {
   before(() => {
     cy.visit("index.html")
+    cy.window().its("jQuery").should("be.a", "function")
     cy.window().its("Centrifuge").should("be.a", "function")
   })
 
@@ -19,59 +21,87 @@ describe("Integration tests page", () => {
     cy.get("#connect-status").should("have.text", "connected")
   })
 
-  it("subscribes", () => {
-    const ns = Cypress.env("CENTRIFUGO_NAMESPACE")
-    const data = {
-      namespaceURI: "urn:open62541.server.application",
-      nodes: ["the.answer", 2345],
-    }
-    cy.get("#channel-input").type(`${ns}:integration@${subscriptionInterval}`)
-    cy.get("#data-input")
-      .clear()
-      .type(JSON.stringify(data), { parseSpecialCharSequences: false })
-    cy.get("#subscribe-button").click()
-    cy.get("#subscription-status").should("have.text", "subscribed")
-    let initialDate
-    cy.get("#publication")
-      .children("li")
-      .as("publications")
-      .should("have.length", 2)
-      .its(0)
-      .should("have.text", "42")
-    cy.get("@publications").should(($li) => {
-      initialDate = new Date($li[1].innerText)
-      expect(initialDate).to.be.above(new Date(0))
+  context("OPC-UA data subscription", () => {
+    it("subscribes", () => {
+      const data = {
+        namespaceURI: "urn:open62541.server.application",
+        nodes: ["the.answer", 2345],
+      }
+      cy.get("#channel-input").type(`${ns}:integration@${subscriptionInterval}`)
+      cy.get("#data-input")
+        .clear()
+        .type(JSON.stringify(data), { parseSpecialCharSequences: false })
+      cy.get("#subscribe-button").click()
+      cy.get("#subscription-status").should("have.text", "subscribed")
+      let initialDate
+      cy.get("#publication")
+        .children("li")
+        .as("publications")
+        .should("have.length", 2)
+        .its(0)
+        .should("have.text", "42")
+      cy.get("@publications").should(($li) => {
+        initialDate = new Date($li[1].innerText)
+        expect(initialDate).to.be.above(new Date(0))
+      })
+      cy.wait(subscriptionInterval)
+      cy.get("@publications")
+        .should("have.length", 2)
+        .its(0)
+        .should("have.text", "42")
+      cy.get("@publications").should(($li) => {
+        expect(new Date($li[1].innerText)).to.be.above(initialDate)
+      })
     })
-    cy.wait(subscriptionInterval)
-    cy.get("@publications")
-      .should("have.length", 2)
-      .its(0)
-      .should("have.text", "42")
-    cy.get("@publications").should(($li) => {
-      expect(new Date($li[1].innerText)).to.be.above(initialDate)
+
+    it("unsubscribes", () => {
+      cy.get("#unsubscribe-button").click()
+      cy.get("#subscription-status").should("have.text", "unsubscribed")
+      let currentTime
+      cy.get("#publication")
+        .children("li")
+        .as("publications")
+        .then(($li) => {
+          currentTime = $li[1].innerText
+        })
+      cy.wait(subscriptionInterval)
+      cy.get("@publications")
+        .should("have.length", 2)
+        .its(0)
+        .should("have.text", "42")
+      cy.get("@publications")
+        .its(1)
+        .should(($li) => {
+          expect($li).to.have.text(currentTime)
+        })
     })
   })
 
-  it("unsubscribes", () => {
-    cy.get("#unsubscribe-button").click()
-    cy.get("#subscription-status").should("have.text", "unsubscribed")
-    let currentTime
-    cy.get("#publication")
-      .children("li")
-      .as("publications")
-      .then(($li) => {
-        currentTime = $li[1].innerText
-      })
-    cy.wait(subscriptionInterval)
-    cy.get("@publications")
-      .should("have.length", 2)
-      .its(0)
-      .should("have.text", "42")
-    cy.get("@publications")
-      .its(1)
-      .should(($li) => {
-        expect($li).to.have.text(currentTime)
-      })
+  context("heartbeat channel", () => {
+    it("subscribes", () => {
+      cy.get("#heartbeat-channel-input").type(`${ns}:heartbeat`)
+      cy.get("#heartbeat-subscribe-button").click()
+      cy.get("#heartbeat-subscription-status").should("have.text", "subscribed")
+      cy.wait(parseInt(Cypress.env("HEARTBEAT_INTERVAL")) * 2)
+      cy.get("#heartbeat-publications")
+        .children()
+        .as("publications")
+        .should("have.length.greaterThan", 1)
+        .and(($rows) => {
+          expect($rows.find('[data-value-for-key="status"]')).to.contain("1")
+          expect($rows.find('[data-value-for-key="description"]')).to.contain(
+            "OPC-UA connected"
+          )
+        })
+    })
+
+    it("unsubscribes", () => {
+      cy.get("#heartbeat-unsubscribe-button").click()
+      cy.get("#heartbeat-subscription-status").should(
+        "have.text",
+        "unsubscribed"
+      )
+    })
   })
 })
 
