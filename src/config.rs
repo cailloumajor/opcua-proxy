5ba90537fdfe5b3b6073ecdf17f30c40ceda32e3
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-04-21 12:29:43 +0000

    fix(deps): upgrade trillium crates
    Needed a `cargo update`.

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,19 @@
 use anyhow::{anyhow, Context as _};
 use tracing::{debug, info, instrument};
+use trillium_client::Client;
+use trillium_tokio::ClientConfig;
 use url::Url;
 
 use crate::model::ConfigFromApi;
-
-type Conn = trillium_client::Conn<'static, trillium_tokio::TcpConnector>;
 
 #[instrument(skip_all)]
 pub(crate) async fn fetch_config(api_url: &Url, partner_id: &str) -> anyhow::Result<ConfigFromApi> {
     let config_url = api_url
         .join(partner_id)
         .context("error joining config API URL and partner ID")?;
-    let mut response = Conn::get(config_url)
+    let client = Client::new(ClientConfig::default());
+    let mut response = client
+        .get(config_url)
         .await
         .context("tags configuration request error")?;
     let response_status = response.status().expect("missing response status code");
